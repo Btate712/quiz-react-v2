82613592ac8e0d6f0d64de351e23e272093b85e4
btate712 <btate712@gmail.com>
2020-06-10 22:29:58 -0500

    added selectedProjects stateful hook

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getProjects } from '../actions/projectActions';
 
 const HomePage = () => {
   const user = useSelector(state => state.user);
   const projects = useSelector(state => state.projects);
+
   const dispatch = useDispatch();
+  
+  const [selectedProjects, setSelectedProjects] = useState(["a project"]);
 
   useEffect(() => {
     dispatch(getProjects(user.token));
   }, []);
+
+  const updateProjectsList = () => {
+    setSelectedProjects(currentSelectedProjects => [...currentSelectedProjects, "and another"]);
+  }
 
   return (
     <div className="home-container">
@@ -17,6 +24,11 @@ const HomePage = () => {
       <h2>Welcome {user.name ? user.name : "loading"}</h2>
       <h3>Projects:</h3>
       { projects.projectList.map(project => project.name) }
+      <br />
+      <button onClick={updateProjectsList}>Click Me!</button>
+      <br />
+      <h3>Selcted Projects:</h3>
+      { selectedProjects.map(project => <li>{project}</li>) }
     </div>
   )
 }
