b2f7b7e563078cb59fd517f900f58715e827de8b
btate712 <btate712@gmail.com>
2020-06-10 22:20:35 -0500

    accessed projects from redux store

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,16 +4,19 @@ import { getProjects } from '../actions/projectActions';
 
 const HomePage = () => {
   const user = useSelector(state => state.user);
+  const projects = useSelector(state => state.projects);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getProjects(user.token));
-  });
+  }, []);
 
   return (
     <div className="home-container">
       <h1>Home Page...</h1>
       <h2>Welcome {user.name ? user.name : "loading"}</h2>
+      <h3>Projects:</h3>
+      { projects.projectList.map(project => project.name) }
     </div>
   )
 }
